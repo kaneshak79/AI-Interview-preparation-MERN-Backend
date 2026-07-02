e24c435c59545ab0d5b60623a4afc672b3f09916
Kanesha K <kaneshak79@gmail.com>
2026-07-02 09:07:20 +0530

    Uncomment createOrder function and update logic
    Uncommented createOrder function and added logic to retrieve amount from request body.

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,45 +1,4 @@
-// import razorpay from "../config/razorpay.js";
-// import crypto from "crypto";
-// import User from "../models/User.js";
 
-// // CREATE ORDER
-// export const createOrder = async (req, res) => {
-//   const options = {
-//     amount: 19900, // ₹199
-//     currency: "INR",
-//     receipt: "receipt_" + Date.now(),
-//   };
-
-//   const order = await razorpay.orders.create(options);
-//   res.json(order);
-// };
-
-// // VERIFY PAYMENT
-// export const verifyPayment = async (req, res) => {
-//   const {
-//     razorpay_order_id,
-//     razorpay_payment_id,
-//     razorpay_signature,
-//   } = req.body;
-
-//   const sign = razorpay_order_id + "|" + razorpay_payment_id;
-
-//   const expectedSign = crypto
-//     .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
-//     .update(sign.toString())
-//     .digest("hex");
-
-//   if (expectedSign === razorpay_signature) {
-//     // ✅ upgrade user
-//     await User.findByIdAndUpdate(req.user.id, {
-//       plan: "pro",
-//     });
-
-//     res.json({ success: true });
-//   } else {
-//     res.status(400).json({ success: false });
-//   }
-// };
 
 import razorpay from "../config/razorpay.js";
 import crypto from "crypto";
@@ -47,22 +6,7 @@ import User from "../models/User.js";
 
 // ✅ CREATE ORDER
 
-// export const createOrder = async (req, res) => {
-//   try {
-//     const options = {
-//       amount: 19900, // ₹199
-//       currency: "INR",
-//       receipt: "receipt_" + Date.now(),
-//     };
 
-//     const order = await razorpay.orders.create(options);
-
-//     res.json(order);
-//   } catch (err) {
-//     console.log("CREATE ORDER ERROR:", err);
-//     res.status(500).json({ msg: "Order creation failed" });
-//   }
-// };
 
 export const createOrder = async (req, res) => {
   try {
