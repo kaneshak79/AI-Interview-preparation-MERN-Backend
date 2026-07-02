214d8a31efb15d10ef5ecdfb36cb34ca7f9f6ee2
Kanesha K <kaneshak79@gmail.com>
2026-07-02 09:11:52 +0530

    Enable AI routes for question generation and evaluation
    Uncommented code to enable AI routes for generating questions and evaluating answers.

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,11 +1,4 @@
-// import express from "express";
-// import { generateQuestions } from "../controllers/aiController.js";
 
-// const router = express.Router();
-
-// router.post("/generate", generateQuestions);
-
-// export default router;
 
 
 import express from "express";
