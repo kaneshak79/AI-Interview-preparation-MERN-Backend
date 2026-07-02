475f5a9285d8cf6a096d61ed9ebead1804546367
Kanesha K <kaneshak79@gmail.com>
2026-07-02 09:06:37 +0530

    Clean up uploadFile function by removing comments
    Removed commented-out code for the uploadFile function.

--- controllers/media.controller.js
+++ controllers/media.controller.js
@@ -1,46 +1,5 @@
 import cloudinary from "../config/cloudinary.js";
 
-// export const uploadFile = async (req, res) => {
-//   try {
-//     if (!req.files || !req.files.file)
-//       return res.status(400).json({ msg: "No file uploaded" });
-
-//     const file = req.files.file;
-
-//     // upload to cloudinary
-//     const result = await cloudinary.uploader.upload(file.tempFilePath, {
-//       folder: "ai-interview"
-//     });
-
-//     res.json({
-//       url: result.secure_url
-//     });
-//   } catch (err) {
-//     res.status(500).json({ msg: err.message });
-//   }
-// };
-
-// export const uploadFile = async (req, res) => {
-//   try {
-//     console.log("FILES:", req.files); // DEBUG
-
-//     if (!req.files || !req.files.file) {
-//       return res.status(400).json({ msg: "No file uploaded" });
-//     }
-
-//     const file = req.files.file;
-
-//     const result = await cloudinary.uploader.upload(file.tempFilePath, {
-//       folder: "ai-interview"
-//     });
-
-//     res.json({ url: result.secure_url });
-
-//   } catch (err) {
-//     res.status(500).json({ msg: err.message });
-//   }
-// };
-
 export const uploadFile = async (req, res) => {
   try {
     console.log("FILES:", req.files);
